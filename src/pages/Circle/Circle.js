3cd1263e2668133a4565155e46ab07e7b89bf6a3
Ian Perez <iperez@mit.edu>
2020-09-19 16:31:17 -0400

    Added link to invite friends

--- src/pages/Circle/Circle.js
+++ src/pages/Circle/Circle.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import { NavLink } from "react-router-dom";
 
-import './Circle.css'; 
+import './Circle.css';
 
 import { Avatar, Button } from '@material-ui/core';
 
@@ -18,32 +18,34 @@ const Circle = (props) => {
     return (
         <div>
             <h1> Your Circle </h1>
-            <a> Invite friends </a>
+            <NavLink style={{textDecoration: "none"}} to="/inviteFriends">
+                <a> Invite friends </a>
+            </NavLink>
 
-            <div className="CircleContainer"> 
-                <div className="AvatarContainer"> 
+            <div className="CircleContainer">
+                <div className="AvatarContainer">
                     <img src={avatar_1}/>
                     <h1> Name </h1>
                 </div>
 
-                <div className="AvatarContainer"> 
+                <div className="AvatarContainer">
                     <img src={avatar_2}/>
                     <h1> Name </h1>
                 </div>
 
-                <div className="AvatarContainer"> 
+                <div className="AvatarContainer">
                     <img src={avatar_3}/>
                     <h1> Name </h1>
                     <h3> It looks like "" may not be doing well today <a href="#"> Reach out? </a> </h3>
                 </div>
 
-                <div className="AvatarContainer"> 
+                <div className="AvatarContainer">
                     <img src={avatar_4}/>
                     <h1> Name </h1>
                 </div>
 
             </div>
-            <img src={circleImg}/> 
+            <img src={circleImg}/>
         </div>
     );
 }
